b7d019d56b20088e8cebeb7d690e6889cbdc6d09
Kshitij Gupta <kgupta119@gmail.com>
2017-08-28 22:16:12 +0530

    Updated gitignore

--- os-shell.c
+++ os-shell.c
@@ -6,10 +6,14 @@ int main() {
 	char *line;
 	char **args;
 	while (1) {
+		// Get command
 		print_prompt();
 		line = line_read();
+
+		// Parse command
 		args = string_tokenizer(line);
 		
+		// Execute command
 		int i = 0;
 		while (args[i] != NULL) {
 			printf("%s\n", args[i]);
